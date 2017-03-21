80b0123bc149a1d5831bde53c5c643844dce4211
infinitu1327 <infinitu1327@gmail.com>
2017-03-21 22:48:54 +0200

    Добавьте файлы проекта.

---
+++ Notifications/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VkNet;
+using System.Net.Mail;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace Notifications
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        VkApi Vk;
+        VkNet.Model.WallGetObject wallPosts;
+        long? OwnerId;
+
+        public Form1(VkApi vk)
+        {
+            Vk = vk;
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Timer t = new Timer()
+            {
+                Interval = 5000
+            };
+            t.Tick += T_Tick;
+
+            StreamReader fileResponseStream = new StreamReader(
+                HttpWebRequest
+                .CreateHttp($"https://api.vk.com/method/groups.getById?" +
+                $"group_id={textBox1.Text.Split('/').Last()}&" +
+                $"v=5.63")
+                .GetResponseAsync()
+                .Result
+                .GetResponseStream()
+                );
+
+            JObject fileResponseJSON = JObject.Parse(fileResponseStream.ReadToEnd());
+            OwnerId = long.Parse(fileResponseJSON["response"][0]["id"].ToString());
+
+            wallPosts = Vk.Wall.Get(new VkNet.Model.RequestParams.WallGetParams()
+            {
+                Count = 2,
+                OwnerId = -OwnerId
+            });
+            t.Start();
+            label3.Visible = true;
+        }
+
+        private void T_Tick(object sender, EventArgs e)
+        {
+            var res = Vk.Wall.Get(new VkNet.Model.RequestParams.WallGetParams()
+            {
+                OwnerId=-OwnerId,
+                Count = 2
+            });           
+            
+            if (res.WallPosts[1].Date != wallPosts.WallPosts[1].Date)
+            {
+                var client = new SmtpClient("smtp.gmail.com", 587)
+                {
+                    Credentials = new NetworkCredential("notidicationsender@gmail.com", "we34n8xza"),
+                    EnableSsl = true
+                };
+                client.Send("notidicationsender@gmail.com", textBox2.Text, "Новый пост в группе " + textBox1.Text, $"Ссылка на пост: https://vk.com/{textBox1.Text.Split('/').Last()}?w=wall" + res.WallPosts[0].ToString().Substring(4));
+                wallPosts = res;
+            }            
+        }
+    }
+}
---
+++ Notifications/Login.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VkNet;
+
+namespace Notifications
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            VkApi vk = new VkApi();            
+
+            try
+            {
+                vk.Authorize(new ApiAuthParams()
+                {
+                    Login = textBox1.Text,
+                    Password = textBox2.Text,
+                    ApplicationId = 5591430,
+                    Settings = VkNet.Enums.Filters.Settings.Wall
+                });
+
+                new Form1(vk).Show();
+                Hide();
+            }
+            catch (Exception)
+            {
+                label3.Text = "Неправильный логин или пароль";
+            }
+        }
+    }
+}
